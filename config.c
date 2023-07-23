41ac61397d69c349565d95c4f884be7413e334fc
imatsatsos <ipmatsatsos@gmail.com>
2023-07-24 00:02:09 +0300

    updated blocks

--- config.c
+++ config.c
@@ -3,13 +3,13 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"$HOME/.config/i3/scripts/kblayout", 3,  0},
-    {"$HOME/.local/bin/status/cputemp",   5,  0},
-    {"$HOME/.local/bin/status/ram",      13,  8},
-    {"$HOME/.local/bin/status/wifi",     29,  9},
-    {"$HOME/.local/bin/status/volume",   27,  11},
-    {"$HOME/.local/bin/status/battery",  29,  0},
-    {"date '+%a %d, %H:%M'",	          5,  0}
+    {"border=#6aa6e6 $HOME/.local/bin/status/kbdlayout", 0,  7},
+    {"border=#6aa6e6 $HOME/.local/bin/status/cputemp",   5,  0},
+    {"border=#6aa6e6 $HOME/.local/bin/status/ram",      13,  8},
+    {"border=#6aa6e6 $HOME/.local/bin/status/wifi",    29,  9},
+    {"border=#6aa6e6 $HOME/.local/bin/status/volume",   0,  11},
+    {"border=#6aa6e6 $HOME/.local/bin/status/battery", 59,  0},
+    {"date '+%a %d, %H:%M'",	                         5,  0}
 };
 
 const unsigned short blockCount = LEN(blocks);
