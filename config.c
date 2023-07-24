8c837c2d2e736f5a581ee6fdace7d17e0af56a3f
imatsatsos <ipmatsatsos@gmail.com>
2023-07-24 09:40:11 +0300

    added cpu usage block

--- config.c
+++ config.c
@@ -5,11 +5,12 @@
 Block blocks[] = {
     {"border=#6aa6e6 $HOME/.local/bin/status/kbdlayout", 0,  7},
     {"border=#6aa6e6 $HOME/.local/bin/status/cputemp",   5,  0},
+    {"border=#6aa6e6 $HOME/.local/bin/status/cpu",       3,  0},
     {"border=#6aa6e6 $HOME/.local/bin/status/ram",      13,  8},
-    {"border=#6aa6e6 $HOME/.local/bin/status/wifi",    29,  9},
-    {"border=#6aa6e6 $HOME/.local/bin/status/volume",   0,  11},
-    {"border=#6aa6e6 $HOME/.local/bin/status/battery", 59,  0},
-    {"date '+%a %d, %H:%M'",	                         5,  0}
+    {"border=#6aa6e6 $HOME/.local/bin/status/wifi",     29,  9},
+    {"border=#6aa6e6 $HOME/.local/bin/status/volume",    0,  11},
+    {"border=#6aa6e6 $HOME/.local/bin/status/battery",  59,  0},
+    {"date '+%a %d, %H:%M'",                             5,  0}
 };
 
 const unsigned short blockCount = LEN(blocks);
