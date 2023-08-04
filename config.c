65721753495c65e5c941c9dab2cff6447616e55d
imatsatsos <ipmatsatsos@gmail.com>
2023-08-04 22:05:37 +0300

    delimiter changes

--- config.c
+++ config.c
@@ -3,15 +3,15 @@
 #include "util.h"
 
 Block blocks[] = {
-  {"border=#6aa6e6 $HOME/.local/bin/status/kbdlayout",  0, 7},
-  {"border=#6aa6e6 $HOME/.local/bin/status/cputemp",    5, 1},
-  {"border=#6aa6e6 $HOME/.local/bin/status/cpu",        3, 2},
-  {"border=#6aa6e6 $HOME/.local/bin/status/ram",       13, 8},
-  {"border=#6aa6e6 $HOME/.local/bin/status/bluetooth", 39, 4},
-  {"border=#6aa6e6 $HOME/.local/bin/status/wifi",      29, 9},
-  {"border=#6aa6e6 $HOME/.local/bin/status/volume",     0, 11},
-  {"border=#6aa6e6 $HOME/.local/bin/status/battery",   59, 3},
-  {"date '+%a %d, %H:%M'",                              5, 0}
+  {"$HOME/.local/bin/status/kbdlayout",  0, 7},
+  {"$HOME/.local/bin/status/cputemp",    5, 1},
+  {"$HOME/.local/bin/status/cpu",        3, 2},
+  {"$HOME/.local/bin/status/ram",       13, 8},
+  {"$HOME/.local/bin/status/bluetooth", 39, 4},
+  {"$HOME/.local/bin/status/wifi",      29, 9},
+  {"$HOME/.local/bin/status/volume",     0, 11},
+  {"$HOME/.local/bin/status/battery",   59, 3},
+  {"$HOME/.local/bin/status/time",       5, 5},
 };
 
 const unsigned short blockCount = LEN(blocks);
--- config.h
+++ config.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define CLICKABLE_BLOCKS  1     // Enable clickability for blocks
-#define CMDLENGTH         100   // Trim block output to this length
-#define DELIMITER         "  " // Delimiter string used to separate blocks
+#define CMDLENGTH         70   // Trim block output to this length
+#define DELIMITER         " "   // Delimiter string used to separate blocks
 #define LEADING_DELIMITER 1     // Whether a leading separator should be used
